48cb8dadd7e71948d4cf1934abb5bde01dcba3bc
Sheng <1272825721@qq.com>
2025-06-29 22:13:40 +0900

    add images

--- src/pages/Bitcoin101Page.tsx
+++ src/pages/Bitcoin101Page.tsx
@@ -2,12 +2,15 @@ import './Bitcoin101Page.css'
 import bitcoinArticle from '../content/bitcoin101Article'
 
 const Bitcoin101Page:React.FC = () =>{
-    const paragraphs  = bitcoinArticle  .paragraph
+    const paragraphs  = bitcoinArticle.paragraph
                         .split(/\n\s*\n/)
                         .map(p  => p.trim())
                         .filter(p => p.length>0);
 
+
     return  (
+        <div>
+        <header className='header'></header>
         <div className='article'> 
             <header className="article-header">
                 <h1 className="title">{bitcoinArticle.title}</h1>
@@ -15,10 +18,17 @@ const Bitcoin101Page:React.FC = () =>{
             </header>
 
             <main  className="article-body">
-               {paragraphs.map((para, idx) =>(
-                <p key={idx}>{para}</p>
-               ))}
+               {paragraphs.map((para, idx) => {
+                const image = bitcoinArticle.imageMap?.[para as keyof typeof bitcoinArticle.imageMap];
+                if(image){
+                    return <img key={idx} src={image.src} alt={image.alt} 
+                    className='article-image' />
+                }
+                return <p key={idx} className='paragraph'>{para}</p>
+            })}
             </main>
+         </div>
+         <footer className='footer'></footer>
          </div>
     )
 }
